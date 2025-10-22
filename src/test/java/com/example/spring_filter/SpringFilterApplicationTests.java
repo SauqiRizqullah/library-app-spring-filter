1c077b0104495e197b309ed0c0d1951af03e3c88
SauqiRizqullah <sauqi.risqullah@gmail.com>
2025-10-22 20:50:11 +0700

    [INIT] Initiate Project

---
+++ src/test/java/com/example/spring_filter/SpringFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_filter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
